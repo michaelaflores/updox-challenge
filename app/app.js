8aa7a9b5b27e76f615e61acafe0960aebf294ad4
michaelaflores <floresm2@miamioh.edu>
2016-11-14 00:51:17 -0500

    Create provider; cleanup from seed
    Added create provider functionality. Removed some unneeded files from
    the seed project.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
   'myApp.version',
 ])
 .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,7 +8,27 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [ '$scope', '$http', function ($scope, $http) {
+  // scoped vars and functions
+  $scope.createProvider = createProvider;
+  $scope.provider = {};
   $http({ method: 'GET', url: '/data.json' }).success((data, status, headers, config) => {
     $scope.providers = data;
+    console.log($scope.providers);
   });
+
+  // function declarations
+  function createProvider(form) {
+    var blob = {};
+    var providersCopy = $scope.providers;
+    blob.last_name = form.lastname;
+    blob.first_name = form.firstname;
+    blob.email_address = form.emailaddress;
+    blob.specialty = form.specialtyname;
+    blob.practice_name = form.practicename;
+
+    $scope.providers = [blob];
+    console.log($scope.providers);
+    $scope.providers = $scope.providers.concat(providersCopy);
+    console.log($scope.providers);
+  }
 }]);
