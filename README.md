e8dfdf7e3014fc8254ad5f5c4a0680a2a1781e60
michaelaflores <floresm2@miamioh.edu>
2016-11-14 00:22:57 -0500

    initial commit
    1-hr mark for review

---
+++ README.md
@@ -0,0 +1,7 @@
+# Updox UI Challenge
+Built on top of angular-seed to save time on basic project setup tasks.
+
+## Getting Started
+1. npm install
+
+2. npm start
---
+++ app/view1/view1.js
@@ -0,0 +1,14 @@
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl',
+  });
+}])
+
+.controller('View1Ctrl', [ '$scope', '$http', function ($scope, $http) {
+  $http({ method: 'GET', url: '/data.json' }).success((data, status, headers, config) => {
+    $scope.providers = data;
+  });
+}]);
